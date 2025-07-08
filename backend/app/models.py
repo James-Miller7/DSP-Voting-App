a6eceed01be937a983124b07a999792f60b7143b
James C Miller <james.miller.1363@gmail.com>
2025-07-08 14:28:26 -0400

    Added pydantic models

---
+++ backend/app/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class VoteCreate(BaseModel):
+  candidate_id: str
+  vote_value: bool
+
+class CandidateCreate(BaseModel):
+  name: str
+  answer: Optional[str] = None
+
+class UserProfile(BaseModel):
+  email: EmailStr
+  id: str
+  role: str
+
+class CandidateOut(BaseModel):
+  id: str
+  name: str
+  answer: Optional[str] = None
+
+
+
+
